6a3e4b91e36676e2eb535792947c576120a7a8a9
Abdul Ghafoor <aghafoor@NIS-PK612s-MacBook-Pro.local>
2024-07-09 20:00:52 +0500

    OK

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+function App() {
+  const [buttonText1, setButtonText1] = useState('Students');
+  const [buttonText2, setButtonText2] = useState('Students Details');
+
+  const handleClick1 = () => {
+    setButtonText1('Button 1 Clicked');
+  };
+
+  const handleClick2 = () => {
+    setButtonText2('Button 2 Clicked');
+  };
+  return (
+    <div className="App">
+      <h1>DevOps King</h1>
+      <button style={{ marginRight: '10px' }} onClick={handleClick1}>{buttonText1}</button>
+      <button style={{ marginRight: '10px' }} onClick={handleClick2}>{buttonText2}</button>
+    </div>
+  );
+}
+
+export default App;
